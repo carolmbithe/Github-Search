36c51fa7a21066d86516fe3ac370b8fbd4dc1170
carolmbithe <carombithe97@gmail.com>
2018-09-21 12:46:43 +0300

    initial commit

---
+++ src/app/github/github.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github',
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css']
+})
+export class GithubComponent implements OnInit {
+
+  title="GITHUB SEARCH"
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
