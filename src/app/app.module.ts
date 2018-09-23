c16a811c884e654e62f1d9244754763768d686e5
carolmbithe <carombithe97@gmail.com>
2018-09-23 15:57:25 +0300

    updated the class constructor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
+import { GithubsearchComponent } from './githubsearch/githubsearch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubComponent
+    GithubComponent,
+    GithubsearchComponent
   ],
   imports: [
     BrowserModule
--- src/app/repository-class/repository.ts
+++ src/app/repository-class/repository.ts
@@ -1,2 +1,5 @@
 export class Repository {
+    constructor( public data:any){
+
+    }
 }
