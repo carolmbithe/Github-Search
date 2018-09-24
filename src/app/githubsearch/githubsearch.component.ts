cae7fc11934ac2624812836469a35de302133d11
carolmbithe <carombithe97@gmail.com>
2018-09-24 09:30:48 +0300

    added a search function

--- src/app/githubsearch/githubsearch.component.ts
+++ src/app/githubsearch/githubsearch.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import { Http } from '@angular/http'
+import { Response, Http } from '@angular/http';
+
 
 
 @Component({
@@ -9,13 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubsearchComponent implements OnInit {
 
+  link = 'https://api.github.com/users/'
+  access = "?access_token=b59fabe37ecb2e26f1de386caf86258a96fd57a4"
+  users = [];
 
 
-  constructor() {
+
+  constructor(private http: Http) {
+    this.http=http;
+  }
+
+    searchUsers(search: HTMLInputElement) {
+      let fullLink = this.link + search.value;
+      this.http.get(fullLink).subscribe((res: Response) => {
+        this.users = res.json();
+  
+      })
   
 
-   }
    
+   
+  }
 
   ngOnInit() {
   }
